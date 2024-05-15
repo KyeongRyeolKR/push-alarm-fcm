57400861d31313c13161d95cd5b0a08b7c5a5f04
김경렬 <rlarudfuf802@naver.com>
2024-05-15 17:03:15 +0900

    push alarm project init

---
+++ src/main/java/com/example/pushmodule/PushModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pushmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pushmodule/PushModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pushmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
